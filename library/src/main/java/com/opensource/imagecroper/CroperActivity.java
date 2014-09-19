d4fa502e78f908b4744b0db69870be70d37b62cf
yinglovezhuzhu <yinglovezhuzhu@139.com>
2014-09-19 15:17:10 +0800

    裁剪Activity界面优化

--- library/src/main/java/com/opensource/imagecroper/CroperActivity.java
+++ library/src/main/java/com/opensource/imagecroper/CroperActivity.java
@@ -19,16 +19,49 @@
 package com.opensource.imagecroper;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.DisplayMetrics;
+import android.view.Window;
 
 import com.opensource.imagecroper.widget.CropView;
 
 public class CroperActivity extends Activity {
 
-	private String tag = CroperActivity.class.getSimpleName();
+	private static final String TAG = "CroperActivity";
+
+    public static final int CROP_MSG = 10;
+    public static final int CROP_MSG_INTERNAL = 100;
+
+
+    private final Handler mHandler = new Handler();
+
+    private boolean mCircleCrop = false;
+    // These options specifiy the output image size and whether we should
+    // scale the output to fit it (or just crop it).
+    private int mAspectX;
+    private int mAspectY; // CR: two definitions per line == sad panda.
+    private int mOutputX;
+    private int mOutputY;
+    private boolean mScale;
+    private boolean mScaleUp = true;
+    private Uri mSaveUri = null;
+    // These are various options can be specified in the intent.
+    private Bitmap.CompressFormat mOutputFormat = Bitmap.CompressFormat.JPEG; // only
+    // used
+    // with
+    // mSaveUri
+
+    boolean mSaving; // Whether the "save" button is already clicked.
+
+    private ContentResolver mContentResolver;
+
+    private Bitmap mBitmap;
+    private Uri mInputUri;
+
 
 	private CropView mCropView = null;
 	
@@ -37,48 +70,83 @@ public class CroperActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+
 		setContentView(R.layout.activity_croper);
-		
-		getWindowManager().getDefaultDisplay().getMetrics(mMetrics);
-		
-		mCropView = (CropView) findViewById(R.id.cv_photo);
-		
-		BitmapFactory.Options options = new BitmapFactory.Options();
-		options.inJustDecodeBounds = true;
-		BitmapFactory.decodeStream(getResources().openRawResource(R.raw.pic1), null, options);
-		options.inJustDecodeBounds = false;
-		options.inSampleSize = options.outWidth / mMetrics.widthPixels;
-		Bitmap bitmap = BitmapFactory.decodeStream(getResources().openRawResource(R.raw.pic1), null, options);
-		mCropView.setImageBitmap(bitmap);
+
+//        Intent intent = getIntent();
+//        Bundle extras = intent.getExtras();
+//
+//        if (extras != null) {
+//            if (extras.getString(CroperConfig.EXTRA_CIRCLE_CROP) != null) {
+//                mCircleCrop = true;
+//                mAspectX = 1;
+//                mAspectY = 1;
+//                //TODO Add some code to make fomat is png when crop a circle image.
+//            }
+//            mSaveUri = extras.getParcelable(CroperConfig.EXTRA_OUTPUT);
+//            if (mSaveUri != null) {
+//                String outputFormatString = extras.getString(CroperConfig.EXTRA_OUTPUT_FORMAT);
+//                if (outputFormatString != null) {
+//                    mOutputFormat = Bitmap.CompressFormat.valueOf(outputFormatString);
+//                }
+//            }
+//            mBitmap = extras.getParcelable(CroperConfig.EXTRA_DATA);
+//            mAspectX = extras.getInt(CroperConfig.EXTRA_ASPECT_X);
+//            mAspectY = extras.getInt(CroperConfig.EXTRA_ASPECT_Y);
+//            mOutputX = extras.getInt(CroperConfig.EXTRA_OUTPUT_X);
+//            mOutputY = extras.getInt(CroperConfig.EXTRA_OUTPUT_Y);
+//            mScale = extras.getBoolean(CroperConfig.EXTRA_SCALE, true);
+//            mScaleUp = extras.getBoolean(CroperConfig.EXTRA_SCALE_UP_IF_NEEDED, true);
+//        }
+//
+//        if (mBitmap == null) {
+//            // Create a MediaItem representing the URI.
+//            mInputUri = intent.getData();
+//
+//            File imageFile = Util.parseUriToFile(this, mInputUri);
+//
+//
+//            if(null != imageFile) {
+//                String imagePath = imageFile.getPath();
+//                Log.i(TAG, "Parse Uri to file, file path : " + imagePath);
+//                BitmapFactory.Options options = new BitmapFactory.Options();
+//                options.inJustDecodeBounds = true;
+//                options.inJustDecodeBounds = false;
+//                options.inSampleSize = 4;
+//                mBitmap = BitmapFactory.decodeFile(imagePath, options);
+//
+//            }
+//        }
+//
+//        if (mBitmap == null) {
+//            Log.e(TAG, "Cannot load bitmap, exiting.");
+//            finish();
+//            return;
+//        }
+//
+//        // Make UI fullscreen.
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+//        findViewById(R.id.discard).setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                setResult(RESULT_CANCELED);
+//                finish();
+//            }
+//        });
+//
+//        findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                onSaveClicked();
+//            }
+//        });
+//
+//
+//        mImageView.setImageBitmapResetBase(mBitmap, true);
+//
+//        makeCropView();
 	}
 	
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		return true;
-//	}
-//
-//	@Override
-//	public boolean onOptionsItemSelected(MenuItem item) {
-//		switch (item.getItemId()) {
-//		case R.id.action_cut:
-//			LogUtil.e(tag, "ImageCut");
-//			if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-//				try {
-//					String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/image.jpg";
-//					mCropView.cutImageBitmap(path);
-//					Toast.makeText(this, "已保存至" + path, Toast.LENGTH_SHORT).show();
-//				} catch (FileNotFoundException ex){
-//					ex.printStackTrace();
-//					Toast.makeText(this, "保存失败", Toast.LENGTH_SHORT).show();
-//				}
-//			} else {
-//				Toast.makeText(this, "请插入Sdcard", Toast.LENGTH_SHORT).show();
-//			}
-//			break;
-//		default:
-//			break;
-//		}
-//		return super.onOptionsItemSelected(item);
-//	}
-	
+
 }
